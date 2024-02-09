e44f3041f1d4864d79ddf13a8d697c5cbb139070
hyonun <119800605+hyonun321@users.noreply.github.com>
2024-02-09 23:05:16 +0900

    Title: 불안한 무빙워크, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240209/불안한 무빙워크/unstable-moving-walk.py
@@ -0,0 +1,31 @@
+from collections import deque
+n,k = map(int,input().split())
+maps = list(map(int,input().split()))
+
+
+front_moving_walk = deque()
+under_moving_walk = deque()
+
+def moving_walk_rotate()
+    return
+
+
+def people_walking()
+    return
+
+
+def add_people()
+    return
+
+answer = 0
+
+while True :
+    answer += 1 
+    if check_zero_pan(k) : break
+    moving_walk_rotate()
+    people_walking()
+    add_people()
+
+
+
+print(answer)
