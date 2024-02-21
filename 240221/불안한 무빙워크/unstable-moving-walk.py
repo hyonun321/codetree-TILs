7435d74ae7b5351606077527a50c4f0b5a169133
hyonun <119800605+hyonun321@users.noreply.github.com>
2024-02-21 15:27:32 +0900

    Title: 불안한 무빙워크, Time: 112ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240221/불안한 무빙워크/unstable-moving-walk.py
+++ 240221/불안한 무빙워크/unstable-moving-walk.py
@@ -1,16 +1,22 @@
+#import sys
+
+#sys.stdin = open("불안한무빙워크.txt", 'r')
 n, k = map(int, input().split())
 maps = list(map(int, input().split()))
 rail_position = {}
+rail_people= {}
 rail_health = {}
 # 레일번호, 레일안정성, 레일 현 위치, 프론트/언더 유무
 rail = []
 for i in range(n) :
     rail_position[i+1] = i+1
     rail_health[i+1] = maps[i]
+    rail_people[i+1] = 0
     rail.append(True)
 for j in range(n,n*2) :
     rail_position[j+1] = j+1
     rail_health[j+1] = maps[j]
+    rail_people[j+1] = 0
     rail.append(False)
 
 def moving_walk_rotate():
@@ -27,50 +33,40 @@ def moving_walk_rotate():
     return
 
 def people_walking():
-    for x in range(len(people)) :
+    for x in rail_people :
         possible = True
-        a,b = people[x] # 사람번호, 현재 레일 번호
-        if b == -100 : continue
-        next_b = (b)%(2*n) +1
+        b = rail_people[x]  # x번째 레일에 사람이있나없나
 
-        # 사람이있나 체크
-        for k in range(len(people)) :
-            a1,b1= people[k]
-            if b1 == next_b :
+        if b == 1 :
+            next_b = (x)%(2*n) +1
+
+            # 사람이있나 체크
+            if rail_people[next_b] == 1 or rail_health[next_b] == 0 :
                 possible = False
-        # 체력이 0 인가 체크
-        if rail_health[next_b] == 0 :
-            possible = False
-        else :
-            if possible :
-                health = rail_health[next_b]
-                health -= 1
-                rail_health[next_b] = health
-        if possible : # 만약 옮길 수 있다면?
-            people[x] = (a,next_b)
+            else :
+                if possible :
+                    health = rail_health[next_b]
+                    health -= 1
+                    rail_health[next_b] = health
+                if possible : # 만약 옮길 수 있다면?
+                    rail_people[x] = 0
+                    rail_people[next_b] = 1
     return
 
-people = []  # (i번째 사람 ,j번째 레일 위)
-p_number = 1
 
 def add_people():
-    global p_number
-    possible = True
+
     for k in range(n*2):
-
+        possible = True
         if rail_position[k+1] == 1 :
-
-            for z in range(len(people)) :
-                a,b = people[z]
-                if b == -100 : continue
-                if b == k+1 : #1위치에있는 레일에 사람이있다?
-                    possible = False
+            if rail_people[k+1] == 1 : #1위치에있는 레일에 사람이있다?
+                possible = False
             if possible and rail_health[k+1] != 0:
                 health = rail_health[k+1]
                 health -=1
                 rail_health[k+1] = health
-                people.append((p_number, k+1)) # 지금 1번자리에있는 번호가들어가야함.
-                p_number += 1
+                rail_people[k+1] = 1 # 지금 1번자리에있는 번호가들어가야함.
+
 
     return
 
@@ -86,14 +82,9 @@ def check_zero_pan(k):
 
 
 def check_position_n() :
-    global people
-    temp= []
-    for k in range(len(people)) :
-        a,b = people[k]
-
-        if rail_position[b] != n :
-            temp.append((a,b))
-    people = temp.copy()
+    for keys in (rail_people):
+        if rail_position[keys] == n and rail_people[keys] == 1 : # b의 레일위치가 n이고, 해당레일에 사람이있다면,
+            rail_people[keys] = 0
 
     return
 
