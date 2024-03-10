8827088326078a83d60b51715eec7ac9cf024c7d
hyonun <119800605+hyonun321@users.noreply.github.com>
2024-03-11 01:55:16 +0900

    Title: T 제거하기, Time: 3730ms, Memory: 79MB, Status: Passed - CodeTree

--- 240311/T 제거하기/remove-t.py
+++ 240311/T 제거하기/remove-t.py
@@ -1,41 +1,22 @@
 import sys
 #sys.stdin = open('T제거하기.txt','r')
+def remove_T_efficiently(a, b):
+    stack = []
+    len_b = len(b)
+    
+    for char in a:
+        stack.append(char)
+        # 현재 스택의 마지막 부분이 b와 일치하는지 확인
+        if stack[-len_b:] == b:
+            # 일치한다면, b의 길이만큼 스택에서 제거
+            del stack[-len_b:]
+            
+    return ''.join(stack)
 
+# 입력 예시
 a = list(input())
 b = list(input())
 
-
-def simulation():
-    global a, b
-    
-    while True:
-        check_i = 1e6
-        #print(len(a),'dhkdn')
-        for i in range(len(a)):
-            if a[i] == b[0]:
-                point = 0
-                #print(i)
-                possible = True
-                if i+len(b) > len(a) : continue
-                for k in range(len(b)):
-                    
-                    if a[i+k] == b[k] : continue
-                    else : 
-                        possible = False
-                        break
-                    
-                if possible:
-                    check_i = i
-                    break
-        #print(check_i)
-        if check_i == 1e6: break
-        else :
-            for _ in range(len(b)):
-                a.pop(check_i)
-        #print(a)
-        
-    return
-
-simulation()
-for k in a:
-    print(k,end='')
+# 효율적인 함수를 사용하여 결과 출력
+result = remove_T_efficiently(a, b)
+print(result)
