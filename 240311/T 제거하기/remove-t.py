883afc2e41d397c76ce24a8ea0af4aed2ce72029
hyonun <119800605+hyonun321@users.noreply.github.com>
2024-03-11 01:46:06 +0900

    Title: T 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240311/T 제거하기/remove-t.py
+++ 240311/T 제거하기/remove-t.py
@@ -1,3 +1,6 @@
+import sys
+#sys.stdin = open('T제거하기.txt','r')
+
 a = list(input())
 b = list(input())
 
@@ -6,15 +9,15 @@ def check_ok():
     global a,b
     #print('check',a,b)
     if len(a) < len(b): return False
+    #print(len(a),len(b))
     for i in range(len(a)):
             if a[i] == b[0]:
                 point = 0
                 for k in range(len(b)):
-                    if a[i+k] == b[k] : 
-                        point += 1 
-                if point == len(b): # 다 들어맞는다면 
-                    check_i = i
-                    return True
+                    #print(i+k,k)
+                    if a[i+k] != b[k] : break
+                check_i = i
+                return True
     return False
 
 def simulation():
@@ -26,15 +29,23 @@ def simulation():
             if a[i] == b[0]:
                 point = 0
                 #print(i)
+                possible = True
+                if i+len(b) > len(a) : continue
                 for k in range(len(b)):
-                    if a[i+k] == b[k] : 
-                        point += 1 
-                if point == len(b): # 다 들어맞는다면 
+                    
+                    if a[i+k] == b[k] : continue
+                    else : 
+                        possible = False
+                        break
+                    
+                if possible:
                     check_i = i
                     break
         #print(check_i)
-        for _ in range(len(b)):
-            a.pop(check_i)
+        if check_i == 1e6: break
+        else :
+            for _ in range(len(b)):
+                a.pop(check_i)
         #print(a)
         
     return
