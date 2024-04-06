d8fa7e4be693af023b81bb8b4518e5cf102bf378
hyonun <119800605+hyonun321@users.noreply.github.com>
2024-04-07 00:39:55 +0900

    Title: 가장 많은 데이터, Time: 365ms, Memory: 33MB, Status: Passed - CodeTree

--- 240407/가장 많은 데이터/most-frequent-data.py
+++ 240407/가장 많은 데이터/most-frequent-data.py
@@ -5,10 +5,10 @@ for _ in range(n):
     a =input()
     if not a in d:
         d[a]=1
-        m=1
+        
     else :
         d[a] += 1
-        if m < d[a]:
-            m=d[a]
+    if m < d[a]:
+        m=d[a]
 
 print(m)
