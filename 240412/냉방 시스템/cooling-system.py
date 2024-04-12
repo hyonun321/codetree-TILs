67afbef66bc01e945dfd6b59150559502c7492dd
hyonun <119800605+hyonun321@users.noreply.github.com>
2024-04-12 22:46:39 +0900

    Title: 냉방 시스템, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240412/냉방 시스템/cooling-system.py
+++ 240412/냉방 시스템/cooling-system.py
@@ -6,21 +6,25 @@ N, M, K = map(int, input().split())
 maps = []
 wind = []
 wall_arr = [[[] for _ in range(N)] for _ in range(N)]
+wall_1st_arr = []
 cool = [[0 for _ in range(N)] for _ in range(N)]
 visited = [[False for _ in range(N)] for _ in range(N)]
+dx = [0, -1, 0, 1]
+dy = [-1, 0, 1, 0]
 for k1 in range(N):
     strings = list(map(int, input().split()))
 
     for k2 in range(len(strings)):
         k3 = strings[k2]
         if 2 <= k3 <= 5:
-            wind.append((k1, k2, k3))
+            wind.append((k1, k2, k3 - 2))
     maps.append(strings)
 
 for wall in range(M):
     wx, wy, ws = map(int, input().split())
     wx -= 1
     wy -= 1
+    wall_1st_arr.append((wx, wy, ws))
     wall_arr[wx][wy].append(ws)
     # 0 은 위 1은 왼쪽
 
@@ -29,87 +33,71 @@ def in_range(x, y):
     return 0 <= x < N and 0 <= y < N
 
 
+# 내가있는곳이 막혀있을때.
+# 내가 가야하는곳이 막혀있을때.
+cant_go = []
+
+
+def make_wall():
+    for x, y, z in wall_1st_arr:
+        if z == 0:  # 바로위
+            nx, ny = x + dx[1], y + dy[1]
+            cant_go.append([(x, y), (nx, ny)])
+            cant_go.append([(nx, ny), (x, y)])
+        if z == 1:  # 바로왼쪽
+            nx, ny = x + dx[0], y + dy[0]
+            cant_go.append([(x, y), (nx, ny)])
+            cant_go.append([(nx, ny), (x, y)])
+    print(cant_go)
+
+    return
+
+
+make_wall()
+
+
 def spread(power, x, y, d):
     global cool
-    straight_block1 = -1
-    if d == 0:  # 좌
-        straight_block1 = 1
-
-    elif d == 1:  # 상
-        straight_block1 = 0
-    elif d == 2:  # 우
-        pass
-    elif d == 3:  # 하
-        straight_block1 = 0
-        pass
-    if straight_block1 in wall_arr[x][y]:
-        cool[x][y] += power
-        visited[x][y] = True
-        return
-    cool[x][y] += power
+    # 나는 대각의 임시 공간이다.
+    # 나는 방향만 체크하고 직선을 다음칸으로 보낸다.
+    # 대각선 반시계
     visited[x][y] = True
-
-    # 대각선 1
-    # 왼 위 오 아
-    straight_block2 = -1
-    # 벽체크
-    dd = (d - 1)%4
-    if dd == 0:  # 좌
-        straight_block2 = 1
-    elif dd == 1:  # 상
-        straight_block2 = 0
-    elif dd == 2:  # 우
-        straight_block2 = 1
-        pass
-    elif dd == 3:  # 하
-        straight_block2 = 0
-        pass
+    cool[x][y] += power
+    dd = (d - 1) % 4
     d1x, d1y = x + dx[dd], y + dy[dd]
-    if in_range(d1x, d1y) and visited[d1x][d1y] == False:
-
-        if not straight_block2 in wall_arr[d1x][d1y]:
+    if not [(x, y), (d1x, d1y)] in cant_go:
+        if in_range(d1x, d1y) and visited[d1x][d1y] == False:
             dd1x, dd1y = d1x + dx[d], d1y + dy[d]
-            if in_range(dd1x, dd1y) and visited[dd1x][dd1y] == False:
-                spread(power - 1, dd1x, dd1y, d)
-    # 대각선 2
-    dd = (d + 1)%4
+            if not [(d1x,d1y),(dd1x,dd1y)] in cant_go:
+                if in_range(dd1x, dd1y) and visited[dd1x][dd1y] == False:
+                    spread(power - 1, dd1x, dd1y, d)
+    # 대각선 시계
+    dd = (d + 1) % 4
     d2x, d2y = x + dx[dd], y + dy[dd]
-    straight_block3 = -1
-    if dd == 0:  # 좌
-        pass
-    elif dd == 1:  # 상
-        straight_block3 = 0
-    elif dd == 2:  # 우
-        straight_block3 = 1
-        pass
-    elif dd == 3:  # 하
-        straight_block3 = 0
-        pass
     # d방향에 따라 다르게 해줘야함.
-    if in_range(d2x, d2y) and visited[d2x][d2y] == False:
-        # 벽체크
-        if not straight_block3 in wall_arr[d2x][d2y]:
+    print(not [(d2x, d2y), (x, y)] in cant_go)
+    if not [(x, y), (d2x, d2y)] in cant_go:
+        if in_range(d2x, d2y) and visited[d2x][d2y] == False:
             dd2x, dd2y = d2x + dx[d], d2y + dy[d]
-            if in_range(dd2x, dd2y) and visited[dd2x][dd2y] == False:
-                spread(power - 1, dd2x, dd2y, d)
+            if not [(d2x,d2y),(dd2x,dd2y)] in cant_go:
+                if in_range(dd2x, dd2y) and visited[dd2x][dd2y] == False:
+                    spread(power - 1, dd2x, dd2y, d)
 
     # 직선
     nx, ny = x + dx[d], y + dy[d]
-    if in_range(nx, ny) and visited[nx][ny] == False:
-        spread(power - 1, nx, ny, d)
+    if not [(x, y), (nx, ny)] in cant_go:
+        if in_range(nx, ny) and visited[nx][ny] == False:
+            spread(power - 1, nx, ny, d)
     return
 
 
 # 0이 위쪽벽
 # 1 이 왼쪽벽
-dx = [0, -1, 0, 1]
-dy = [-1, 0, 1, 0]
 
 
 def blow_up():
     global visited
     for wx, wy, wd in wind:
-        wd -= 2
         power = 5
         visited = [[False for _ in range(N)] for _ in range(N)]
         wx, wy = wx + dx[wd], wy + dy[wd]
@@ -155,7 +143,7 @@ def side_cold_down():
             cool[i][0] -= 1
         if cool[i][N - 1] > 0:
             cool[i][N - 1] -= 1
-    for j in range(1,N-1):
+    for j in range(1, N - 1):
         if cool[0][j] > 0:
             cool[0][j] -= 1
         if cool[N - 1][j] > 0:
