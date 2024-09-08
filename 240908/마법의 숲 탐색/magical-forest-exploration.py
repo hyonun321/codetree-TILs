31b3238c7d2f07335a8b50d4562d1ad7682c8379
hyonun <119800605+hyonun321@users.noreply.github.com>
2024-09-08 17:25:45 +0900

    Title: 마법의 숲 탐색, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240908/마법의 숲 탐색/magical-forest-exploration.py
@@ -0,0 +1,160 @@
+from collections import deque
+
+R,C,K = map(int,input().split())
+board = [ [0 for _ in range(C)] for _ in range(R+1)]
+gdx=[-1,0,1,0]
+gdy=[0,1,0,-1]
+fairy_position= [(-1,-1) for _ in range(K+1)]
+result = 0
+LEFT,RIGHT=0,1
+g_start = [-1]
+g_exit = [-1]
+
+
+for idx_i in range(K):
+    ci,di = map(int,input().split())
+    g_start.append(ci-1)
+    g_exit.append(di)
+def print_b():
+    for k in board:
+        print(*k)
+
+# 차례대로 골렘 추가
+  # 이때 위치에 넣어질수 없으면 초기화
+# 각 골렘 아래로 이동
+  # 방향 변경시 골렘 출구 돌리기
+  # 서쪽 먼저 탐색 while각
+# 가장 남쪽으로 이동
+
+def init_board():
+    global board
+    board = [ [0 for _ in range(C)] for _ in range(R+1)]
+
+
+def bfs(x,y):
+    dx=[-1,0,1,0]
+    dy=[0,1,0,-1]
+    queue = deque()
+    visited = [[False for _ in range(C)]for _ in range(R+1)]
+    visited[x][y] = True
+    queue.append((x,y,board[x][y]))
+    max_x = x
+    while queue:
+        rx,ry,r_idx = queue.popleft()
+        exit_x,exit_y = fairy_position[r_idx]
+        exit_x =  exit_x + gdx[g_exit[r_idx]]
+        exit_y =  exit_y + gdy[g_exit[r_idx]]
+        exit_possible = False
+        if ( rx == exit_x and ry == exit_y) :
+            exit_possible = True
+        for d in range(4):
+            nx,ny = rx+dx[d],ry+dy[d]
+            if (in_range(nx,ny) and board[nx][ny] == r_idx and visited[nx][ny] == False and nx >= x):
+                queue.append((nx,ny,board[nx][ny]))
+                visited[nx][ny] = True
+                max_x = max(max_x,nx)
+            elif exit_possible and (in_range(nx,ny) and board[nx][ny] >0 and visited[nx][ny] == False and nx >= x) :
+                new_fairy_idx = board[nx][ny]
+                queue.append((nx, ny,new_fairy_idx))
+                visited[nx][ny] = True
+                max_x = max(max_x, nx)
+    return max_x
+
+def one_gol_down(idx):
+    g_col = g_start[idx]
+    for row in range(R-1,0,-1):
+        if(check_gol_body(row,g_col)):
+            if(move_left_right(row,g_col,idx)):
+                return True
+            return False
+    return False
+
+def move(x,y,idx) :
+    if (idx == LEFT):
+
+        return (x,y)
+    elif (idx == RIGHT):
+
+        return (x,y)
+    return (-1,-1)
+
+def move_left_right(x,y,g_number):
+    # 여기가 어려운 각
+    while(1):
+        if (check_gol_position(x,y,LEFT)):
+            g_exit[g_number] = (g_exit[g_number] - 1 )%4
+            #print("왼쪽 자리있음",g_number)
+            x = x + 1
+            y = y - 1
+        else :
+            if(check_gol_position(x,y,RIGHT)):
+                g_exit[g_number] = (g_exit[g_number] + 1) % 4
+                fairy_position[g_number] = move(x, y, RIGHT)
+                #print("오른쪽 자리 있음",g_number)
+                x = x + 1
+                y = y + 1
+
+            else : # 제자리
+                #print("제자리", g_number)
+                fairy_position[g_number] = (x, y)
+                if not (check_gol_body(x,y)):
+                    return False
+                break
+
+    board_draw_by_fairy(g_number)
+    #print_b()
+    return True
+    # 그냥 그자리 그대로
+
+def board_draw_by_fairy(g_number):
+    x,y = fairy_position[g_number]
+    for d in range(4):
+        nx,ny = x+gdx[d],y+gdy[d]
+        if (in_range(nx,ny) and board[nx][ny] == 0) :
+            board[nx][ny] = g_number
+    if in_range(x,y) and board[x][y] == 0:
+        board[x][y] = g_number
+
+def check_gol_position(x,y,position):
+    if ( position == LEFT):
+        if(in_range(x,y-2) and board[x][y-2] == 0) and (in_range(x-1,y-1) and board[x-1][y-1]== 0) and (in_range(x+1,y-1) and board[x+1][y-1]== 0) and ((in_range(x + 1, y - 2) and board[x + 1][y - 2] == 0 )) and ((in_range(x + 2, y - 1) and board[x + 2][y - 1] == 0 ) ):
+            return True
+        return False
+
+    elif ( position == RIGHT):
+        if (in_range(x, y + 2) and board[x][y + 2] == 0) and (in_range(x + 1, y + 1) and board[x + 1][y + 1]== 0) and ((in_range(x - 1, y + 1) and board[x - 1][y + 1] == 0 ))and ((in_range(x + 2, y + 1) and board[x + 2][y + 1] == 0 )) and ((in_range(x + 1, y + 2) and board[x + 1][y + 2] == 0 )):
+            return True
+        return False
+
+
+def fairy_move_and_point_up(idx):
+    global result
+    x,y = fairy_position[idx]
+    x = x+gdx[g_exit[idx]]
+    y = y+gdy[g_exit[idx]]
+    bx = bfs(x,y)
+    result += (bx ) # 인덱스 보정
+    return
+
+def in_range(x,y):
+    return 0<=x<R+1 and 0<=y<C
+
+def check_gol_body(x,y):
+    dx=[0,0,1,-1]
+    dy=[1,-1,0,0]
+    for d in range(4):
+        nx,ny = x+dx[d],y+dy[d]
+        if not (in_range(nx,ny) and board[nx][ny] == 0) :
+            return False
+    if not in_range(x,y) and board[x][y] == 0:
+        return False
+    return True
+
+for gol_idx in range(1,K+1):
+
+    gol_ok = one_gol_down(gol_idx)
+    if not (gol_ok):
+        init_board()
+        continue
+    fairy_move_and_point_up(gol_idx)
+print(result)
